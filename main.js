76c8daf0fb30a3e01c56fbff1eb75145b10f7d3d
drworm75 <dwgames@gmail.com>
2017-02-11 12:16:58 -0600

    working through math of adding spaces

--- main.js
+++ main.js
@@ -2,9 +2,27 @@ var buildButton = document.getElementById("build");
 
 function buildTree (clickEvent) {
 	var treeSize = document.getElementById("size").value;
-	console.log(treeSize);	
+	// console.log(treeSize);	
 	var treeCharacter = document.getElementById("character").value;
-	console.log(treeCharacter);
+	// console.log(treeCharacter);
+	var printTree = "";
+	
+	for (var i = 0; i < treeSize; i++) {
+
+		for (var j = 0; j < ((treeSize * 2) - 2); j++) {
+			// console.log(treeSize - 1);
+			// console.log("i =" + i);
+			// console.log("j =" + j);
+			console.log(i + j);
+			if (j + i === (treeSize - 1) && (j + i) <= ((treeSize - 1) + j)) {
+				printTree += treeCharacter;
+			} else {
+				printTree += "x";
+				
+			}
+	}
+		console.log(printTree);
+}
 }
 
 
